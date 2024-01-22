4b2f81ac8236e2cc75c69aec4802266db9cb6b65
davidtey <davidteyby@gmail.com>
2024-01-22 17:05:00 +0800

    added radians & facing conversion

--- algo/utils.py
+++ algo/utils.py
@@ -2,6 +2,7 @@ import numpy as np
 
 def grid_to_coords(x_g: int, y_g: int):
     """ Convert grid number to continuous coordinates (assuming position in middle of grid)
+
     Args:
         x_g (int): x coordinate (grid)
         y_g (int): y coordinate (grid)
@@ -25,3 +26,45 @@ def coords_to_grid(x: float, y: float) -> (int, int):
         y_g (int): y coordinate (grid)
     """
     return x//10, y//10
+
+def facing_to_rad(facing: str) -> float:
+    """Convert {N, S, E, W} to radians
+
+    Args:
+        facing (str): direction in {N, S, E, W}
+
+    Returns:
+        float: direction in radians
+    """
+
+    assert facing in ['N', 'S', 'E', 'W']
+
+    if facing == 'E':
+        return 0
+    elif facing == 'N':
+        return np.pi/2
+    elif facing == 'W':
+        return np.pi
+    elif facing == 'S':
+        return -np.pi/2
+    
+def rad_to_facing(rad: float) -> str:
+    """Convert radians to {N, S, E, W}
+
+    Args:
+        rad (float): direction in radians
+
+    Returns:
+        str: direction in {N, S, E, W}
+    """
+
+    assert abs(rad) <= np.pi
+
+    if np.pi/4 < rad <= 3*np.pi/4:
+        return 'N'
+    elif -np.pi/4 < rad <= np.pi/4:
+        return 'E'
+    elif -3*np.pi/4 < rad <= -np.pi/4:
+        return 'S'
+    else:
+        return 'W'
