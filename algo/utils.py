898bcc3b8eea1725dbc163609a5ef1d077a4ffe9
davidtey <davidteyby@gmail.com>
2024-01-22 19:06:30 +0800

    update coords and grid conversion
    grid_to_coords returns bottom left of grid
    changed from 20x20 to 40x40 grid

--- algo/utils.py
+++ algo/utils.py
@@ -1,7 +1,7 @@
 import numpy as np
 
 def grid_to_coords(x_g: int, y_g: int):
-    """ Convert grid number to continuous coordinates (assuming position in middle of grid)
+    """ Convert grid number to continuous coordinates (assuming bottom left of grid)
 
     Args:
         x_g (int): x coordinate (grid)
@@ -12,7 +12,7 @@ def grid_to_coords(x_g: int, y_g: int):
         y (float): y coordinate (continuous)
     """
 
-    return x_g*10 + 5, y_g*10 + 5
+    return x_g*5, y_g*5
 
 def coords_to_grid(x: float, y: float) -> (int, int):
     """Convert continuous coordinates to grid number
@@ -25,7 +25,7 @@ def coords_to_grid(x: float, y: float) -> (int, int):
         x_g (int): x coordinate (grid)
         y_g (int): y coordinate (grid)
     """
-    return x//10, y//10
+    return x//5, y//5
 
 def facing_to_rad(facing: str) -> float:
     """Convert {N, S, E, W} to radians
