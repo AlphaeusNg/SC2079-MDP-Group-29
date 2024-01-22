f386a17818ce9fac6142e9bf75e2f9a36d66a3c5
davidtey <davidteyby@gmail.com>
2024-01-22 19:03:42 +0800

    initialise checkpoint object

---
+++ algo/objects/Checkpoint.py
@@ -0,0 +1,21 @@
+from algo import utils
+
+class Checkpoint:
+    def __init__(self, x: float, y: float, theta: float) -> None:
+        """Checkpoint constructor
+
+        Args:
+            x (float): x coordinate of checkpoint
+            y (float): y coordinate of checkpoint
+            theta (float): desired angle when reaching checkpoint
+
+        Parameters:
+            active (bool): whether checkpoint has been reached
+        """
+
+        self.x = x
+        self.y = y
+        self.theta = theta
+        self.active = True
+
+        
