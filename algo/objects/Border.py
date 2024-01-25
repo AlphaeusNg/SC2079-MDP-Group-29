7f1e5280b5e5df5e1d2f8505803d7e7c03dca501
davidtey <davidteyby@gmail.com>
2024-01-25 14:36:26 +0800

    add VirtualBorderWall object

--- algo/objects/Border.py
+++ algo/objects/Border.py
@@ -10,3 +10,14 @@ class Border(pygame.sprite.Sprite):
         self.rect = self.image.get_rect()
         self.rect.x = x
         self.rect.y = y
+
+class VirtualBorderWall(pygame.sprite.Sprite): 
+    def __init__(self, width, height, x, y): 
+        super().__init__() 
+  
+        self.image = pygame.Surface([width, height]) 
+        self.image.fill((255, 0, 0, 24))
+        self.rect = self.image.get_rect()
+        
+        self.rect.x = x
+        self.rect.y = y
