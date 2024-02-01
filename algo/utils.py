c6fb128c59dedde595a2f78de7a90ca07371589c
davidtey <davidteyby@gmail.com>
2024-02-02 01:20:29 +0800

    added reeds shepp functions

--- algo/utils.py
+++ algo/utils.py
@@ -71,7 +71,7 @@ def rad_to_facing(rad: float) -> str:
     else:
         return 'W'
     
-def coords_to_pixelcoords(x_g:int, y_g:int, map_x0=50, map_y0=50, map_width=800, map_height=800):
+def coords_to_pixelcoords(x_g:int=0, y_g:int=0, x: float=None, y: float=None, map_x0=50, map_y0=50, map_width=800, map_height=800):
     """Convert grid numbers to pixel coordinates
 
     Args:
@@ -82,11 +82,13 @@ def coords_to_pixelcoords(x_g:int, y_g:int, map_x0=50, map_y0=50, map_width=800,
         map_width (int, optional): map width in pixels. Defaults to 800.
         map_height (int, optional): map height in pixels. Defaults to 800.
     """
-    x, y = grid_to_coords(x_g, y_g)
+    if x == None:
+        x, y = grid_to_coords(x_g, y_g)
+
     new_x = map_x0 + x*map_width/200
     new_y = map_y0 + map_height - y*map_height/200
 
-    return new_x, new_y
+    return int(new_x), int(new_y)
 
 def l1 (x1: float, y1: float, x2: float, y2: float) -> float:
     """L1 distance between 2 points in R2
@@ -99,6 +101,11 @@ def l2 (x1: float, y1: float, x2: float, y2: float) -> float:
     """
 
     return math.sqrt((x1 - x2)**2 + (y1 - y2)**2)
+
+def diag_dist(x1: float, y1: float, x2: float, y2: float) -> float:
+    dx = abs(x1 - x2)
+    dy = abs(y1 - y2)
+    return math.sqrt(2*min(dx, dy)**2) + abs(dx - dy)
 
 def normalise_theta(theta: float):
     revolutions = int((theta + np.sign(theta) * np.pi) / (2 * np.pi))
@@ -120,3 +127,38 @@ def truncated_remainder(dividend, divisor):
     remainder = dividend - divisor * divided_number
 
     return remainder
+
+def M(theta):
+    """
+    Return the angle phi = theta mod (2 pi) such that -pi <= theta < pi.
+    """
+    theta = theta % (2*math.pi)
+    if theta < -math.pi: return theta + 2*math.pi
+    if theta >= math.pi: return theta - 2*math.pi
+    return theta
+
+def R(x, y):
+    """
+    Return the polar coordinates (r, theta) of the point (x, y).
+    """
+    r = math.sqrt(x*x + y*y)
+    theta = math.atan2(y, x)
+    return r, theta
+
+def change_of_basis(p1, p2):
+    """
+    Given p1 = (x1, y1, theta1) and p2 = (x2, y2, theta2) represented in a
+    coordinate system with origin (0, 0) and rotation 0 (in degrees), return
+    the position and rotation of p2 in the coordinate system which origin
+    (x1, y1) and rotation theta1.
+    """
+    theta1 = deg_to_rad(p1[2])
+    dx = p2[0] - p1[0]
+    dy = p2[1] - p1[1]
+    new_x = dx * math.cos(theta1) + dy * math.sin(theta1)
+    new_y = -dx * math.sin(theta1) + dy * math.cos(theta1)
+    new_theta = p2[2] - p1[2]
+    return new_x, new_y, new_theta
+
+def deg_to_rad(deg):
+    return math.pi * deg / 180
