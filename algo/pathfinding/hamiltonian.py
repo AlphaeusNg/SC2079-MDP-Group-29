506fc7214da1451f6c51396dd679fd1b11814c66
brendanteo42 <130235553+brendanteo42@users.noreply.github.com>
2024-02-03 23:10:25 +0800

    Add files via upload

---
+++ algo/pathfinding/hamiltonian.py
@@ -0,0 +1,69 @@
+import random
+import constants as c
+from algo.objects.Obstacle import Obstacle
+
+
+def find_nearest_neighbor_path(start, obstacles):
+    path = []
+    current_pos = start
+
+    while obstacles:
+        nearest_neighbor = min(obstacles, key=lambda obstacle: abs(obstacle.x_g - current_pos[0]) + abs(obstacle.y_g - current_pos[1]))
+        obstacles.remove(nearest_neighbor)
+        direction = nearest_neighbor.facing
+        if direction == 'N':
+            path.append((nearest_neighbor.x_g + 10, nearest_neighbor.y_g + 40))
+        elif direction == 'S':
+            path.append((nearest_neighbor.x_g - 10, nearest_neighbor.y_g - 40))
+        elif direction == 'E':
+            path.append((nearest_neighbor.x_g + 40, nearest_neighbor.y_g - 10))
+        elif direction == 'W':
+            path.append((nearest_neighbor.x_g - 40, nearest_neighbor.y_g + 10))
+
+    current_pos = path[-1]
+
+    return path
+
+
+def generate_random_obstacles(grid_size, obstacle_count):
+    obstacles = []
+    directions = ['N', 'S', 'E', 'W']
+
+    while len(obstacles) < obstacle_count:
+        x = random.randint(0, grid_size - 1)
+        y = random.randint(0, grid_size - 1)
+        direction = random.choice(directions)
+        obstacles.append(Obstacle(x,y,direction))
+
+    return obstacles
+
+
+def print_grid(grid_size, obstacles):
+    path = []
+    for y in range(grid_size - 1, -1, -1):
+        for x in range(grid_size):
+            position = (x, y)
+            if (0 <= x <= 2) and (0 <= y <= 2):
+                print("C", end=" ")  # Starting point
+            elif any(obstacle.x_g == x and obstacle.y_g == y for obstacle in obstacles):
+                direction = next((obstacle.facing for obstacle in obstacles if (obstacle.x_g, obstacle.y_g) == position), None)
+                print(direction, end=" " if direction else ".")  # Obstacle facing direction
+            elif (x, y) in path:
+                print("*", end=" ")  # Mark the path with "*"
+            else:
+                print(".", end=" ")  # Empty space
+        print()
+
+
+if __name__ == "__main__":
+    grid = 200
+    obstacle_number = 5
+    start_position = (0, 0)
+
+    obstacles = generate_random_obstacles(grid, obstacle_number)
+    print("\nGrid:")
+    print_grid(grid, obstacles)
+
+    path = find_nearest_neighbor_path(start_position, obstacles)
+    print("\nNearest Neighbor Path:")
+    print(path)
