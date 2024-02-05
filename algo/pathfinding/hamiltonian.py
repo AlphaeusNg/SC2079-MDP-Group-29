1ddb7f0e9b396fde9341af814a32c8dc392187df
brendanteo42 <130235553+brendanteo42@users.noreply.github.com>
2024-02-05 17:40:48 +0800

    Update hamiltonian.py

--- algo/pathfinding/hamiltonian.py
+++ algo/pathfinding/hamiltonian.py
@@ -2,26 +2,15 @@ import random
 from algo.objects.Obstacle import Obstacle
 
 
-def find_nearest_neighbor_path(start, obstacles):
+def find_nearest_neighbor_path(obstacles):
     path = []
-    current_pos = start
+    current_pos = (15, 15)
 
     while obstacles:
         nearest_neighbor = min(obstacles, key=lambda obstacle: abs(obstacle.x_g - current_pos[0] + offset_x(obstacle.facing)) + abs(obstacle.y_g - current_pos[1] + offset_y(obstacle.facing)))
         obstacles.remove(nearest_neighbor)
-        direction = nearest_neighbor.facing
-        if direction == 'N':
-            path.append((nearest_neighbor.x_g + 10, nearest_neighbor.y_g + 40))
-            current_pos = (nearest_neighbor.x_g + 10, nearest_neighbor.y_g + 40)
-        elif direction == 'S':
-            path.append((nearest_neighbor.x_g - 10, nearest_neighbor.y_g - 40))
-            current_pos = (nearest_neighbor.x_g - 10, nearest_neighbor.y_g - 40)
-        elif direction == 'E':
-            path.append((nearest_neighbor.x_g + 40, nearest_neighbor.y_g - 10))
-            current_pos = (nearest_neighbor.x_g + 40, nearest_neighbor.y_g - 10)
-        elif direction == 'W':
-            path.append((nearest_neighbor.x_g - 40, nearest_neighbor.y_g + 10))
-            current_pos = (nearest_neighbor.x_g - 40, nearest_neighbor.y_g + 10)
+        path.append((nearest_neighbor.x_g + offset_x(nearest_neighbor.facing), nearest_neighbor.y_g + offset_y(nearest_neighbor.facing)))
+        current_pos = (nearest_neighbor.x_g + offset_x(nearest_neighbor.facing), nearest_neighbor.y_g + offset_y(nearest_neighbor.facing))
 
     return path
 
@@ -49,12 +38,16 @@ def offset_y(facing):
 
 
 def generate_random_obstacles(grid_size, obstacle_count):
+    if grid_size < 100:
+        offset = 5
+    else:
+        offset = 50
     obstacles = []
     directions = ['N', 'S', 'E', 'W']
 
     while len(obstacles) < obstacle_count:
-        x = random.randint(0, grid_size - 1)
-        y = random.randint(0, grid_size - 1)
+        x = random.randint(offset, grid_size - offset)
+        y = random.randint(offset, grid_size - offset)
         direction = random.choice(directions)
         obstacles.append(Obstacle(x, y, direction))
 
@@ -82,12 +75,11 @@ def print_grid(grid_size, obstacles):
 if __name__ == "__main__":
     grid = 200
     obstacle_number = 5
-    start_position = (0, 0)
 
     obstacles = generate_random_obstacles(grid, obstacle_number)
     print("\nGrid:")
     print_grid(grid, obstacles)
 
-    path = find_nearest_neighbor_path(start_position, obstacles)
+    path = find_nearest_neighbor_path(obstacles)
     print("\nNearest Neighbor Path:")
     print(path)
