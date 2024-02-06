1c31e45909ee7d386115a8a758823a6a5ced4746
brendanteo42 <130235553+brendanteo42@users.noreply.github.com>
2024-02-07 01:08:17 +0800

    Update Testing.py

--- algo/simulator/Testing.py
+++ algo/simulator/Testing.py
@@ -9,5 +9,15 @@ if __name__ == "__main__":
     obstacles2 = [Obstacle(15, 20, 'N'), Obstacle(15, 15, 'S'),
                   Obstacle(20, 15, 'S'), Obstacle(20,20,'N')]
 
-    Simulator(obstacles2).start_simulation()
+    
+    obstacles3 = [Obstacle(10, 17, 'N'), Obstacle(17, 5, 'S'), Obstacle(27, 26, 'W'), Obstacle(29, 26, 'N'), 
+                 Obstacle(16, 17, 'S'), Obstacle(14, 32, 'W'), Obstacle(29, 37, 'N'), Obstacle(27, 3, 'E')]
+
+    obstacles4 = [Obstacle(25, 1, 'S'), Obstacle(37, 5, 'W'), Obstacle(5, 8, 'E'), Obstacle(10, 30, 'S'), 
+                 Obstacle(25, 24, 'W'), Obstacle(5, 17, 'E'), Obstacle(22, 17, 'E'), Obstacle(7,22, 'W')]
+
+    obstacles5 = [Obstacle(4, 38, 'N'), Obstacle(15, 15, 'E'), Obstacle(6, 6, 'S'), Obstacle(10, 30, 'S'), 
+                 Obstacle(25, 24, 'W'), Obstacle(38, 15, 'N'), Obstacle(38, 3, 'W'), Obstacle(38,38, 'N')]
+
+    Simulator(obstacles3).start_simulation()
     
