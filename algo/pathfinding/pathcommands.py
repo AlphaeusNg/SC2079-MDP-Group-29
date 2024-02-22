505fd60fc6c5fd96c028af8f104c9598227ce7fd
brendanteo42 <130235553+brendanteo42@users.noreply.github.com>
2024-02-22 20:39:53 +0800

    Test

--- algo/pathfinding/pathcommands.py
+++ algo/pathfinding/pathcommands.py
@@ -12,20 +12,29 @@ def print_path(path):
             f"Current Node (x:{node.x:.2f}, y: {node.y:.2f}, " + f"theta: {node.theta * 180 / np.pi:.2f}), Action: {node.prevAction}")
 
 
+def distance(prev, node):
+    return np.sqrt((node.x - prev.x) ** 2 + (node.y - prev.y) ** 2)
+
+
 def construct_path(path, L, Radius):
-    LF, SF, RF, LR, SB, RB = 0, 0, 0, 0, 0, 0
+    LF, SF, RF, LB, SB, RB = 0, 0, 0, 0, 0, 0
     approx = 10
     command = []
     droid = []
+    prev = path[0]
+    dis = 0
     for node in path:
         droid.append([round(node.x / approx), round(node.y / approx)])
+        dis += distance(prev, node)
         if node.prevAction == (Gear.FORWARD, Steering.LEFT):
             LF += 1
         else:
             if LF >= 1:
                 LF *= (L / (2 * np.pi * Radius)) * 360
                 command.append(f"LF{int(LF):03d}")
+                command.append(f"SF{int(dis):03d}")
                 LF = 0
+                dis = 0
         # Gear.FORWARD + Steering.STRAIGHT
         if node.prevAction == (Gear.FORWARD, Steering.STRAIGHT):
             SF += 1
@@ -41,15 +50,19 @@ def construct_path(path, L, Radius):
             if RF >= 1:
                 RF *= (L / (2 * np.pi * Radius)) * 360
                 command.append(f"RF{int(RF):03d}")
+                command.append(f"SF{int(dis):03d}")
                 RF = 0
+                dis = 0
         # Gear.REVERSE + Steering.LEFT
         if node.prevAction == (Gear.REVERSE, Steering.LEFT):
-            LR += 1
+            LB += 1
         else:
-            if LR >= 1:
-                LR *= (L / (2 * np.pi * Radius)) * 360
-                command.append(f"LR{int(LR):03d}")
-                LR = 0
+            if LB >= 1:
+                LB *= (L / (2 * np.pi * Radius)) * 360
+                command.append(f"LB{int(LB):03d}")
+                command.append(f"SB{int(dis):03d}")
+                LB = 0
+                dis = 0
         # Gear.REVERSE + Steering.STRAIGHT
         if node.prevAction == (Gear.REVERSE, Steering.STRAIGHT):
             SB += 1
@@ -65,7 +78,10 @@ def construct_path(path, L, Radius):
             if RB >= 1:
                 RB *= (L / (2 * np.pi * Radius)) * 360
                 command.append(f"RB{int(RB):03d}")
+                command.append(f"SB{int(dis):03d}")
                 RB = 0
+                dis = 0
+        prev = node
 
     return command, droid
 
